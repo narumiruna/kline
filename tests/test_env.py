0c01fcf820c518b42c7a7f619e86322321f26e35
narumi <toucans-cutouts0f@icloud.com>
2023-03-22 16:19:56 +0800

    use kucoin to test

--- tests/test_env.py
+++ tests/test_env.py
@@ -3,7 +3,7 @@ from cryptodataset.envs import TradingEnv
 
 def test_tradingenv():
     init_cash = 25000
-    env = TradingEnv(init_cash=init_cash)
+    env = TradingEnv(exchange='KuCoin', init_cash=init_cash)
 
     _ = env.reset()
     done = False
