fa93545f4b9e83650529fbefb1bf39a41a55c6f6
なるみ <toucans-cutouts0f@icloud.com>
2023-03-24 11:32:19 +0800

    remove test cases

--- cryptodataset/test_ccxt.py
+++ cryptodataset/test_ccxt.py
@@ -9,16 +9,6 @@ def ccxt_data() -> CCXTData:
     return CCXTData('KuCoin')
 
 
-def test_ohlcv_get_ohlcv(ccxt_data: CCXTData) -> None:
-    symbol = 'BTC/USDT'
-    timeframe = '1d'
-    limit = 30
-    df = ccxt_data.get_ohlcv(symbol, timeframe, limit=limit)
-
-    assert isinstance(df, pd.DataFrame)
-    assert len(df) > 0
-
-
 def test_ohlcv_get_ohlcv_limit(ccxt_data: CCXTData) -> None:
     symbol = 'BTC/USDT'
     timeframe = '1d'
--- cryptodataset/test_max.py
+++ cryptodataset/test_max.py
@@ -9,16 +9,6 @@ def max_data() -> MAXData:
     return MAXData()
 
 
-def test_max_ohlcv_get_ohlcv(max_data: MAXData) -> None:
-    symbol = 'BTCUSDT'
-    timeframe = '1d'
-    limit = 30
-
-    df = max_data.get_ohlcv(symbol, timeframe, limit=limit)
-    assert isinstance(df, pd.DataFrame)
-    assert len(df) > 0
-
-
 def test_max_ohlcv_get_ohlcv_limit(max_data: MAXData) -> None:
     symbol = 'BTCUSDT'
     timeframe = '1d'
